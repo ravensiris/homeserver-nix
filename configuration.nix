217e7e0e3f7fd0c14b908ed3c55b6c90c4860dcd
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-11-29 01:54:07 +0100

    🔥 Rm node-red

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,6 @@ in {
       ./navidrome.nix
       ./home-assistant.nix
       ./syncthing.nix
-      ./node-red.nix
     ]
     ++ lib.optionals (!lib.inPureEvalMode) ["${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"];
 
