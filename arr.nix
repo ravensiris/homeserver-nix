c1ec9d5f42ba4b988a527703fc38cf499455b296
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-12-09 00:40:42 +0100

    ⚠️ Temporary allow EOL dotnet

--- arr.nix
+++ arr.nix
@@ -11,6 +11,13 @@ with lib; {
   };
 
   config = {
+    nixpkgs.config.permittedInsecurePackages = [
+      "aspnetcore-runtime-6.0.36"
+      "aspnetcore-runtime-wrapped-6.0.36"
+      "dotnet-sdk-6.0.428"
+      "dotnet-sdk-wrapped-6.0.428"
+    ];
+
     services.caddy.virtualHosts."http://jellyfin.komorebi.lan".extraConfig = ''
       reverse_proxy :8096
     '';
